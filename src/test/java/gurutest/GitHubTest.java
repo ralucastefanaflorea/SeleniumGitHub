aa2e88334791f93b58aa982f9df20218f9846182
Raluca Florea <rflorea@murex.com>
2022-10-18 11:31:30 +0300

    Add first test

---
+++ src/test/java/gurutest/GitHubTest.java
@@ -0,0 +1,30 @@
+package gurutest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class GitHubTest {
+    @Test
+    public void testGuru(){
+        System.setProperty("webdriver.chrome.driver","C:\\chromedriver3.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.guru99.com/");
+        try{Thread.sleep(2000);}catch(Exception e){System.out.println(e.getMessage());}
+
+        WebElement blueTitle = driver.findElement(By.cssSelector("h3"));
+        String expected = "Guru99 is totally new kind of learning experience.";
+
+        Assert.assertEquals(blueTitle.getText(),expected);
+
+        try{Thread.sleep(2000);}catch(Exception e){System.out.println(e.getMessage());}
+        driver.close();
+    }
+}
